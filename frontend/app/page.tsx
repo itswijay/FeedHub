560f227373b952efe32bbb317debb9f919224603
Pubudu Wijesundara <neo17641@gmail.com>
2026-01-12 11:11:16 +0530

    Initialize next frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="">
+      <main className=""> 
+        <h1 className="text-6xl">Hello World</h1>
+      </main>
+    </div>
+  )
+}
